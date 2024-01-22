443142b3c57924541fa7e2c83fb1c5342c7fdd59
dev4lentinho <pollavalentinho@gmail.com>
2024-01-22 16:19:18 -0300

    file added

---
+++ python_reference.py
@@ -0,0 +1,30 @@
+
+# TODO: Operadores aritmeticos
+
+# print(6 + 4) #* esto es una suma
+# print(7 - 4) #* esto es una resta
+# print(3 * 4) #* esto es una multiplicación
+# print(11 / 4) #* esto es una división
+# print(3 % 8) #* esto es un porcentaje
+# print(5 ** 4) #* esto es un exponente
+# print(11 // 5) # *esta es una división de flor
+
+# TODO: Operadores comparativos
+
+# print(4 < 9) #* 4 menor que
+# print(5 > 3) #* 5 mayor que
+# print(5 == 4) #* es igual a
+# print(4 <= 5) #* menor o igual a
+# print(4 >= 2) #* mayor o igual a
+# print(4 != 4) #* es diferente a 
+
+# TODO: Operadores logicos.
+
+# print(True and False) #* Si ambos son True devuelve True
+# print(False or True) #* Si uno es true devuelve True
+# print(not(False)) #* Convierte el valor al valor contrario
+
+
+# udndneej
+
+# # # # sexp
